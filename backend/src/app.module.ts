636873772d87296d461c54b9009e73dc54784730
QuangTeoo <116084622+QuangTeoo@users.noreply.github.com>
2024-10-20 14:38:44 +0700

    create folder users,posts,follows,comments

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { CommentsModule } from './comments/comments.module';
+import { FollowsModule } from './follows/follows.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/nettruyen'),
+    UsersModule,
+    PostsModule,
+    CommentsModule,
+    FollowsModule,
   ],
   controllers: [AppController],
   providers: [AppService],

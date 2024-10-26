f0b18e228bf2c2636ad8a7857526bd9eb4fa52b7
QuangTeoo <116084622+QuangTeoo@users.noreply.github.com>
2024-10-26 19:10:29 +0700

    wip

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('/api');
   await app.listen(3000);
-}
+  app.use(
+    session({
+      secret
+    })
+  )
+  }
 bootstrap();
